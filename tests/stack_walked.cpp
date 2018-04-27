39ecc7a8c1a4638a6c8fc822c58eb251ceb14289
Théophile Bastian <contact@tobast.fr>
2018-04-27 11:06:25 +0200

    Use those functions in stack_walked.cpp test

--- tests/stack_walked.cpp
+++ tests/stack_walked.cpp
@@ -3,6 +3,41 @@
 
 #include "../stack_walker/stack_walker.hpp"
 
+void fill_my_stack1(volatile int&);
+void fill_my_stack2(volatile int&);
+
+void stack_filled() {
+    int frame_id = 1;
+    walk_stack([&frame_id](const unwind_context_t& ctx) {
+            Dl_info func_info;
+            int dl_rc = dladdr((void*) ctx.rip, &func_info);
+            printf("#%d - %s - RIP=%lx, RBP=%lx, RSP=%lx\n",
+                    frame_id,
+                    (dl_rc != 0) ? func_info.dli_sname : "[Unknown func]",
+                    ctx.rip,
+                    ctx.rbp,
+                    ctx.rsp);
+        });
+}
+
+void fill_my_stack2(volatile int& depth) {
+    if(depth == 0)
+        stack_filled();
+    else {
+        --depth;
+        fill_my_stack1(depth);
+    }
+}
+
+void fill_my_stack1(volatile int& depth) {
+    if(depth == 0)
+        stack_filled();
+    else {
+        --depth;
+        fill_my_stack2(depth);
+    }
+}
+
 int main() {
     Dl_info main_info;
     int rc = dladdr((void*)&main, &main_info);
@@ -10,6 +45,10 @@ int main() {
     printf("I'm in %s!\n", (rc != 0) ? main_info.dli_sname : "[No data]");
 
     stack_walker_init();
+
+    printf("Stack walker init'd!\n");
+    volatile int depth = 10;
+    fill_my_stack1(depth);
 
     return 0;
 }
