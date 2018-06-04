5661ccbefc7c56b90ecf51178d494960d7902882
Théophile Bastian <contact@tobast.fr>
2018-06-04 15:17:23 +0200

    shared: add typedef for fde func with deref callback

--- shared/context_struct.h
+++ shared/context_struct.h
@@ -4,6 +4,10 @@ typedef struct {
     uintptr_t rip, rsp, rbp;
 } unwind_context_t;
 
-typedef unwind_context_t (*_fde_func_t)(unwind_context_t, uintptr_t);
-
 typedef uintptr_t (*deref_func_t)(uintptr_t);
+
+typedef unwind_context_t (*_fde_func_t)(unwind_context_t, uintptr_t);
+typedef unwind_context_t (*_fde_func_with_deref_t)(
+        unwind_context_t,
+        uintptr_t,
+        deref_func_t);
